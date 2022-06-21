7e0ee8742ae78f062737ba1d1382f29230e93acb
SnailDOS <zarasoonich@hotmail.com>
2022-06-21 11:41:53 +0800

    Upload bubbleid-sdk

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "https://api.snaildos.com",
+});
+
+export interface UserResponse {
+  id: string;
+  username?: string;
+  name?: string;
+  email?: string;
+  avatar?: string;
+}
+
+export class InnaticalID {
+  private appID: string;
+  private token: string;
+
+  constructor(appID: string, token: string) {
+    this.appID = appID;
+    this.token = token;
+  }
+
+  async findUser(
+    query: { id: string } | { email: string } | { username: string }
+  ) {
+    return (
+      await client.post<UserResponse>("/users/login", {
+        token: this.token,
+        ...query,
+      })
+    ).data;
+  }
+
+  async getUserFromToken(token: string) {
+    return (
+      await client.get<UserResponse>("/users/@me", {
+        headers: {
+          authorization: token,
+        },
+      })
+    ).data;
+  }
+
+  createURL(callback: string) {
+    return (
+      // `https://api.snaildos.com/connect` +
+      // new URLSearchParams({ callback, id: this.appID })
+      console.log("BubbleID connect is not integrated yet.")
+    );
+  }
+}
